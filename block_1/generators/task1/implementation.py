b6a177123bd2f1028815ae70258103590e4625bc
Artur Tokranov <a.tokranov@bars-open.ru>
2024-06-26 23:26:17 +0300

    Выполнил задачи generators

--- block_1/generators/task1/implementation.py
+++ block_1/generators/task1/implementation.py
@@ -5,9 +5,9 @@ from typing import (
 
 def fib(amount_of_numbers: int) -> Generator[int, None, None]:
     """Возвращает генератор чисел Фибоначчи."""
+
     num1 = 0
     num2 = 1
-    while amount_of_numbers:
+    for _ in range(amount_of_numbers):
         num1, num2 = num2, num2 + num1
         yield num1
-        amount_of_numbers -= 1
